53fd0439b95e5571fdc6233457ae79ceb1d27fb2
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-21 15:07:07 -0600

    added human class

---
+++ MedievalYou/MedievalYou/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MedievalTextGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MEDIEVAL YOU");
+            Console.WriteLine("What race are you?" +
+                "Human" +
+                "Elf" +
+                "Goblin");
+            if (Console.ReadLine() == "Human")
+            {
+                Human player1 = new Human(Human.CreateNewHuman());
+                Console.WriteLine($"You are a Human. These are your stats: {player1}");
+
+            }
+
+
+
+        }
+    }
+}
